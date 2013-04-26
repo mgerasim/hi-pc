31698be6799ce257d0792674016e8dcd25beca77
Mikahil Gerasimov <GerasimovMN@khv.dv.rt.ru>
2013-04-26 11:25:15 +1100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hipc::Application.config.secret_token = '50ae9dcb154a15cd2b12804b21ce516a296a8aa98e3767c477a0e0eda23686fd73a54f0862fd20d2a31acb0e7050efb518ad57a676be1c2210ed884ca831cc2a'
---
+++ config/initializers/spree.rb
@@ -0,0 +1,23 @@
+#encoding: utf-8
+# Configure Spree Preferences
+#
+# Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
+#       If you would like users to be able to update a setting with the Admin it should NOT be set here.
+#
+# In order to initialize a setting do:
+# config.setting_name = 'new value'
+Spree.config do |config|
+  # Example:
+  # Uncomment to override the default site name.
+  config.site_name = "Интернет-ветрина автомобильных бортовых компьютеров"
+  config.use_s3 = true
+  config.s3_bucket = 'hipc'
+  config.s3_access_key = "AKIAJPNLN2VOL4H5CH5A"
+  config.s3_secret = "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"
+end
+
+Spree.user_class = "Spree::User"
+
+Paperclip.interpolates(:s3_eu_url) do |attachment, style|
+  "#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/}, "")}"
+ end
